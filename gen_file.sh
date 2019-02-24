0a563af893d419e53f7a16885a6296edf8f750a3
tfliao <tingfuliao@gmail.com>
2019-02-24 15:30:29 +0800

    Write a small tool to simply my copy-paste task after contest

---
+++ gen_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Generate camel-style problem name as filename"
+echo "Problem name: "
+read TITLE
+fn=
+for w in $TITLE; do
+    h=$(echo $w | cut -b-1)
+    fn=${fn}${h^^}$(echo $w |cut -b2-)
+done
+fn=${fn}.cpp
+echo "Now read solution and write to $fn, ^D to stop"
+cat > $fn
+
