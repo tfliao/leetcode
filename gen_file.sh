6a80f51a4a416351e1429e603cd5a3b3d9473f2a
tfliao <tingfuliao@gmail.com>
2019-03-03 11:47:26 +0800

    Deal with empty problem name

--- gen_file.sh
+++ gen_file.sh
@@ -8,6 +8,10 @@ for w in $TITLE; do
     h=$(echo $w | cut -b-1)
     fn=${fn}${h^^}$(echo $w |cut -b2-)
 done
+if [ -z "$fn" ]; then
+    echo "Failed to generate file name"
+    exit 1
+fi
 fn=${fn}.cpp
 echo "Now read solution and write to $fn, ^D to stop"
 cat > $fn
